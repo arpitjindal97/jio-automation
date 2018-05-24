edc6284cfaa71ea5e5116e3adffdb55696116fef
Arpit Agarwal <arpitjindal97@gmail.com>
2018-05-24 17:34:18 +0530

    fixed error in windows.go

--- windows.go
+++ windows.go
@@ -13,7 +13,18 @@ func GetChromeDriverPath() string {
 
 	data, _ := box.MustBytes("chromedriver_windows")
 
-	_ := ioutil.WriteFile(os.TempDir()+"/chromedriver", data, 0744)
+	ioutil.WriteFile(os.TempDir()+"/chromedriver", data, 0744)
 
 	return os.TempDir() + "/chromedriver"
+}
+
+func GetChromeBrowserPath() string {
+	if _, err := os.Stat("chrome"); err != nil {
+		path, err := exec.LookPath(browser)
+		if err != nil {
+			panic("Browser binary path not found")
+		}
+		return path
+	}
+	return browser
 }
